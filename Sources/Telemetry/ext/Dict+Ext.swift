974894692b425154aa85189e714edccc39bfe37e
eonist <30n1st@gmail.com>
2023-08-09 18:45:29 +0200

    Update Dict+Ext.swift

--- Sources/Telemetry/ext/Dict+Ext.swift
+++ Sources/Telemetry/ext/Dict+Ext.swift
@@ -2,9 +2,11 @@ import Foundation
 
 extension Dictionary {
    /**
-    * - Fixme: ⚠️️ add doc
-    * - Parameter other: - Fixme: ⚠️️ add doc
-    * - Returns: - Fixme: ⚠️️ add doc
+    * - Fixme: ⚠️️ Add doc
+    * - Fixme: ⚠️️ We could also use reduce like shown here: https://stackoverflow.com/a/75548503/5389500
+    * - Fixme: ⚠️️ We can even add it to a + operator like: `func + <Self>(a: Self, b: Self) -> Self {...}`
+    * - Parameter other: - Fixme: ⚠️️ Add doc
+    * - Returns: - Fixme: ⚠️️ Add doc
     */
    func combinedWith(_ other: [Key: Value]) -> [Key: Value] {
       var dict = self
